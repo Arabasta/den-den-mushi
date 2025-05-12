3de830844500ce4f6f9350364ad03a4181590dc2
Arabasta <keiyamx@gmail.com>
2025-05-12 20:43:33 +0800

    feat: add settings and light theme

---
+++ public/js/settings/themes.js
@@ -0,0 +1,14 @@
+export const themes = {
+    dark: {
+        background: '#282a36',
+        foreground: '#f8f8f2',
+        cursor: '#FFFFFF',
+        selection: '#44475a'
+    },
+    light: {
+        background: '#ffffff',
+        foreground: '#000000',
+        cursor: '#000000',
+        selection: '#c0c0c0'
+    }
+};
